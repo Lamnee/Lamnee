e5f2ac10cf25e51c9ce2b3750a1dfbe5d1f8804f
Lamnee <89033286+Lamnee@users.noreply.github.com>
2021-08-17 01:01:12 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,13 +1,13 @@
 const textConfig = {
 text1: "Hi,chào cậu :3",
   text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
+  text3: "Cậu yêu tớ có phải không nào :3",
   text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
   text5: "Đéo",
   text6: "Yêu Lâmm",
-  text7: "Cho tớ biết lí do cậu thích tớ đi :vv",
+  text7: "Lý do cậu thích tớ :vv",
   text8: "Gửi cho tớ :3",
-  text9: "Vì Lâm đẹp try vlllll",
+  text9: "Vì Lâm đẹp try vlllll :3333",
   text10: "Love u 3000",
   text11:
     "Khi nào hết dịch mình đi chơi nhaaa! Giờ thì nhắn tin cho tớ đi nào :3",
